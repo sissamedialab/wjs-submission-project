ccccb8b66029335c376a251f762e65bf2e79c9ec
Iacopo Spalletti <i.spalletti@nephila.it>
2025-07-22 00:04:31 +0200

    Set dynamic navbar

--- wjs/plugins/wjs_submission/workflow.py
+++ wjs/plugins/wjs_submission/workflow.py
@@ -30,6 +30,11 @@ class Step:
     The name of the view that manages this step
     """
 
+    icon: str
+    """
+    Name of the icon to be used in the navigation bar
+    """
+
     check_function: Callable[[Journal, Article], bool] | None = None
     """
     A generic function to check the availablity of a single step for an article
@@ -166,52 +171,54 @@ def step_check_review_submit(
 
 
 STEPS = {
-    1: Step(
-        step_number=1,
-        label="Start",
-        step_view_name="wjs_submission_1",
-        check_function=None,
-    ),
+    1: Step(step_number=1, label="Start", step_view_name="wjs_submission_1", check_function=None, icon="bi-clipboard"),
     2: Step(
         step_number=2,
         label="Select Issue",
         step_view_name="wjs_submission_2",
         check_function=step_check_select_issue,
+        icon="bi-card-checklist",
     ),
     3: Step(
         step_number=3,
         label="Keywords",
         step_view_name="wjs_submission_3",
         check_function=step_check_keywords,
+        icon="bi-key",
     ),
     4: Step(
         step_number=4,
         label="Authors",
         step_view_name="wjs_submission_4",
         check_function=step_check_authors,
+        icon="bi-person",
     ),
     5: Step(
         step_number=5,
         label="data",
         step_view_name="wjs_submission_5",
         check_function=step_check_metadata,
+        icon="bi-file-earmark-text",
     ),
     6: Step(
         step_number=6,
         label="Files",
         step_view_name="wjs_submission_6",
         check_function=step_check_files,
+        icon="bi-files",
     ),
     7: Step(
         step_number=7,
         label="Access & funding",
         step_view_name="wjs_submission_7",
         check_function=step_check_access_funding,
+        icon="bi-credit-card",
     ),
     8: Step(
         step_number=8,
         label="Review & submit",
         step_view_name="wjs_submission_8",
         check_function=step_check_review_submit,
+        icon="bi-search",
     ),
 }
