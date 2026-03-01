8ed14a62e9b152a00aeb450139c3f3cb73e16a49
Iacopo Spalletti <i.spalletti@nephila.it>
2026-03-01 21:48:21 +0100

    update migrations

--- wjs/plugins/wjs_submission/migrations/0004_articlesubmission_affiliation_country_collaboration_and_more.py
+++ wjs/plugins/wjs_submission/migrations/0004_articlesubmission_affiliation_country_collaboration_and_more.py
@@ -9,7 +9,7 @@ class Migration(migrations.Migration):
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ("core", "0099_alter_accountrole_options"),
-        ("submission", "0085_submissionconfiguration_autocomplete_keywords"),
+        ("submission", "0089_keyword_created_keyword_deactivated_keyword_notes_and_more"),
         ("wjs_submission", "0003_articlesubmission_cover_letter_file"),
     ]
 
--- wjs/plugins/wjs_submission/migrations/0007_accessmode_accessmodejournal_and_more.py
+++ wjs/plugins/wjs_submission/migrations/0007_accessmode_accessmodejournal_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("journal", "0066_issue_allowed_sections_issue_date_close_and_more"),
+        ("journal", "0068_issue_allowed_sections_issue_cached_display_title_pt_and_more"),
         ("wjs_submission", "0006_articlesubmission_administrative_files_and_more"),
     ]
 
