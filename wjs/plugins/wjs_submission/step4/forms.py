ddac28a22f7cb2a0c04d75125ec380a6d6dcec5b
Iacopo Spalletti <i.spalletti@nephila.it>
2026-03-25 22:15:40 +0100

    feat: Refactor Step 4 to split HTMX actions in multiple views

--- wjs/plugins/wjs_submission/step4/forms.py
+++ wjs/plugins/wjs_submission/step4/forms.py
@@ -112,7 +112,7 @@ class AddAuthorForm(forms.ModelForm):
         :param args: Positional arguments passed to the parent form.
         :param kwargs: Keyword arguments; must include 'article_id'.
         """
-        self.is_revision = kwargs.pop("is_revision", False)
+        self.is_revision = kwargs.pop("is_revision")
         self.article = kwargs.pop("article")
         super().__init__(*args, **kwargs)
 
@@ -147,16 +147,140 @@ class AddAuthorForm(forms.ModelForm):
         return instance
 
 
-class AddFrozenAutorForm(forms.Form):
+class AddFrozenAutorObjectForm(forms.Form):
+    """
+    Associate a frozen author to an article.
+
+    Handle the creation or retrieval of a frozen author object associated with a given
+    article and email. This form is intended to manage frozen author objects in the
+    context of articles efficiently. It also allows for customization of form initialization
+    where revision-specific features might be employed.
+
+    :ivar is_revision: Indicates whether the form instance is being used for a revision
+        workflow.
+    :type is_revision: bool
+    :ivar instance: An object representing details about the author, typically passed
+        as an instance to the form.
+    :type instance: Any
+    :ivar article: An object representing the article associated with the frozen author
+        operation.
+    :type article: Any
+    """
+
     def __init__(self, *args, **kwargs):
-        self.is_revision = kwargs.pop("is_revision", False)
+        """
+        Initialize an instance of the class and sets up the provided attributes.
+
+        :param args: Positional arguments passed during initialization.
+        :param kwargs: Keyword arguments passed during initialization. Expected
+            keys include:
+            - is_revision (bool): Indicates if the object is a revision.
+            - instance: Specifies the instance of the object.
+            - article: Specifies the related article.
+        """
+        self.is_revision = kwargs.pop("is_revision")
         self.instance = kwargs.pop("instance")
         self.article = kwargs.pop("article")
         super().__init__(*args, **kwargs)
 
-    def save(self, commit=True):
-        author, created = FrozenAuthor.get_or_snapshot_if_email_found(email=self.instance.email, article=self.article)
-        return author
+    def save(self, commit: bool = True) -> FrozenAuthor:
+        """
+        Save the current instance and associates it with an author snapshot.
+
+        If an email match is found in the database. If no match is found, a new author
+        snapshot is created.
+
+        :param commit: Whether to commit the changes to the database. Defaults to True.
+        :type commit: bool
+        :return: The author instance that was either fetched or newly created.
+        :rtype: FrozenAuthor
+        """
+        frozen_author, __ = FrozenAuthor.get_or_snapshot_if_email_found(
+            email=self.instance.email, article=self.article
+        )
+        return frozen_author
+
+
+class AddCollaborationObjectForm(forms.Form):
+    """
+    Handles the form initialization and saving for adding a collaboration object.
+
+    This class is designed to be used when dealing with collaboration objects,
+    allowing instances to be linked to articles or revisions based on the provided
+    context. It manages the linkage logic and provides a method to save the
+    collaboration to the appropriate model.
+
+    :ivar is_revision: Indicator of whether the collaboration is related to a
+        revision. Determines the model and fields to use during saving.
+    :type is_revision: bool
+    :ivar instance: The collaboration instance to be linked. Represents the main
+        collaboration object being processed by the form.
+    :type instance: Collaboration
+    :ivar article: The article object to which the collaboration is to be linked.
+    :type article: Article
+    :ivar collaboration_relation: Defines the type of relationship or relation
+        between the article and the collaboration instance.
+    :type collaboration_relation: str
+    :ivar user: The user performing the collaboration operation.
+    :type user: User
+    """
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initialize an instance of the class and sets up the provided attributes.
+
+        :param args: Positional arguments to be passed to the superclass initializer.
+        :param kwargs: Keyword arguments, including specific ones for initializing the
+            instance attributes:
+
+            - is_revision: Indicates whether the entity represents a revision.
+            - instance: Represents the instance of the related object.
+            - article: Associated article object.
+            - collaboration_relation: Denotes the relationship for collaboration.
+            - user: User associated with the operation.
+        """
+        self.is_revision = kwargs.pop("is_revision")
+        self.instance = kwargs.pop("instance")
+        self.article = kwargs.pop("article")
+        self.collaboration_relation = kwargs.pop("collaboration_relation")
+        self.user = kwargs.pop("user")
+        super().__init__(*args, **kwargs)
+
+    def save(self, commit: bool = True) -> RevisionArticleCollaboration | ArticleCollaboration:
+        """
+        Associate the collaboration instance to the articl.
+
+        It creates or retrieves the appropriate collaboration
+        link based on whether the instance is associated with a revision or not. The function
+        decides the correct model (either RevisionArticleCollaboration or ArticleCollaboration)
+        to use based on the `is_revision` attribute. It then utilizes the foreign key filters
+        to retrieve or create a collaboration link and set default attributes where necessary.
+
+        :param commit: Flag indicating whether to persist the save operation immediately
+                       after creating or retrieving the collaboration instance.
+                       Defaults to `True`.
+        :type commit: bool
+        :return: The created or retrieved `RevisionArticleCollaboration` or
+                 `ArticleCollaboration` instance, based on the `is_revision` attribute.
+        :rtype: RevisionArticleCollaboration | ArticleCollaboration
+        """
+        model, fk_filter = (
+            (
+                RevisionArticleCollaboration,
+                {"revision_storage": RevisionStorage.objects.get(article=self.article)},
+            )
+            if self.is_revision
+            else (ArticleCollaboration, {"article": self.article})
+        )
+        collaboration_link = model.objects.get_or_create(
+            **fk_filter,
+            collaboration=self.instance,
+            defaults={
+                "relation": self.collaboration_relation,
+                "order": self.instance.next_collaboration_sort(article=self.article, revision=self.is_revision),
+            },
+        )
+        return collaboration_link  # noqa: RET504
 
 
 class AddCollaborationForm(forms.ModelForm):
@@ -193,12 +317,12 @@ class AddCollaborationForm(forms.ModelForm):
         :param kwargs: Keyword arguments; must include 'article_id',
                        'collaboration_relation', and 'user'.
         """
-        article_id = kwargs.pop("article_id")
-        self.is_revision = kwargs.pop("is_revision", False)
-        self.article = Article.objects.get(pk=article_id)
-        self.collaboration_relation = kwargs.pop("collaboration_relation", False)
+        self.is_revision = kwargs.pop("is_revision")
+        self.article = kwargs.pop("article")
+        self.collaboration_relation = kwargs.pop("collaboration_relation")
         self.user = kwargs.pop("user")
         super().__init__(*args, **kwargs)
+
         self.fields["collaboration_relation"].initial = self.collaboration_relation
         for field in self.fields:
             if self.fields[field].required:
