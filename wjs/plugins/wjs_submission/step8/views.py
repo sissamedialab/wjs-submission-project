8c4e9fadd08504d3b33a5f7bdf924753f0f472f2
Iacopo Spalletti <i.spalletti@nephila.it>
2026-03-25 23:21:22 +0100

    feat: add support for affiliations

--- wjs/plugins/wjs_submission/step8/views.py
+++ wjs/plugins/wjs_submission/step8/views.py
@@ -196,7 +196,7 @@ class SubmissionStep8View(AuthorFilteringView, StepCheckView, UpdateView):
             context["article_data"].special_request = self.object.submission_data.special_request
             context["article_data"].cover_letter_file = self.object.submission_data.cover_letter_file
             context["correspondence_author"] = self.object.correspondence_author
-            context["affiliation_country"] = self.object.submission_data.affiliation_country
+            context["affiliation"] = self.object.submission_data.affiliation
             if is_revision(self.object):
                 if title := self.object.revisionstorage.data.get("title"):
                     context["article_data"].title = title
