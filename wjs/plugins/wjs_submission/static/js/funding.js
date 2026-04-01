25e6f4e6f3bde7e8ca0ccbd539a60d2403ef3fb0
Iacopo Spalletti <i.spalletti@nephila.it>
2026-04-01 03:45:53 +0200

    fix: refactor funding modal management to improve robustness

--- wjs/plugins/wjs_submission/static/js/funding.js
+++ wjs/plugins/wjs_submission/static/js/funding.js
@@ -1,4 +1,3 @@
-
 function updateFundingFieldsStatus() {
   document.getElementById("add-funding-btn").disabled = false;
 }
@@ -8,3 +7,41 @@ function setupFundingSelection() {
     updateFundingFieldsStatus();
   });
 }
+
+function checkAccessMode(value) {
+  if (value === "1") {
+    document.getElementById("js-access-mode-message").classList.remove("d-none");
+  } else {
+    document.getElementById("js-access-mode-message").classList.add("d-none");
+  }
+}
+document.addEventListener("DOMContentLoaded", function() {
+  if (accessModeSelector) {
+    checkAccessMode(accessModeSelector.value);
+    accessModeSelector.addEventListener("change", function(event) {
+      checkAccessMode(event.target.value);
+    });
+  }
+});
+
+window.addEventListener("DOMContentLoaded", function() {
+  document.body.addEventListener("close-active-modal", function() {
+    document.querySelector("#htmxModal").querySelector("[aria-label=Close]").click();
+  });
+  document.body.addEventListener("open-active-modal", function() {
+    const htmxModal = new bootstrap.Modal(document.getElementById('htmxModal'))
+    htmxModal.show()
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  const modal = document.getElementById("htmxModal");
+  modal.addEventListener("hidden.bs.modal", function() {
+    document.getElementById("htmxModalContent").innerHTML = "";
+  });
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  setupFundingSelection();
+});
